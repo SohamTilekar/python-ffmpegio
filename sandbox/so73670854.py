b972ec6f36ab59e038da86f7f4b388db209989d3
Takeshi Ikuma (LSUHSC) <tikuma@lsuhsc.edu>
2022-09-11 19:03:58 -0500

    fixed so73670854

--- sandbox/so73670854.py
+++ sandbox/so73670854.py
@@ -28,9 +28,19 @@ ffmpegio.ffmpeg("-y -f lavfi -i testsrc=size=640x480:rate=30:duration=5 sandbox/
 #     "[w0][w1][w2][w3][w4][w5][w6][w7][w8][w9][w10][w11][w12][w13][w14]mix=inputs=15:weights=1 -r 30 sandbox/out.mp4"
 # )
 cmd = (
-    r"-y -i sandbox/in.mp4 -vf setpts=\'floor(N/15)/30\',select=\'mod(n,15)+1\':n=15[v0][v1][v2][v3][v4][v5][v6][v7][v8][v9][v10][v11][v12][v13][v14];"
+    r"-loglevel debug -y -i sandbox/in.mp4 -vf setpts=\'floor(N/15)/30/TB\',select=\'mod(n,15)+1\':n=15[v0][v1][v2][v3][v4][v5][v6][v7][v8][v9][v10][v11][v12][v13][v14];"
     "[v0][v1][v2][v3][v4][v5][v6][v7][v8][v9][v10][v11][v12][v13][v14]mix=inputs=15:weights=1 -r 30 sandbox/out.mp4"
 )
+
+# P = 5
+
+# vlabels = "".join((f"[v{i}]" for i in range(P)))
+# wlabels = "".join((f"[w{i}]" for i in range(P)))
+# setpts = ";".join((f"[v{i}]setpts=\'N/30*TB\'[w{i}]" for i in range(P)))
+# cmd = (
+#     rf"-loglevel debug -y -i sandbox/in.mp4 -vf select=\'mod(n,{P})+1\':n={P}{vlabels};"
+#     f"{setpts};{wlabels}mix=inputs={P}:weights=1 -r 30 sandbox/out.mp4"
+# )
 
 ffmpegio.ffmpeg(cmd)
 
